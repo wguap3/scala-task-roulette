29008b767e6f7c0eb61750aa98a5f8f6969d921a
3wguap <rnbrell@yandex.by>
2026-04-11 01:30:36 +0400

    add model

---
+++ src/main/scala/domain/Models.scala
@@ -0,0 +1,60 @@
+package domain
+
+import java.util.UUID
+import java.time.Instant
+
+case class User(
+               id: UUID,
+               name: String,
+               createdAt: Instant
+               )
+
+case class Room(
+               id: UUID,
+               name: String,
+               createdBy: UUID,
+               createdAt: Instant
+               )
+
+sealed trait RoomRole
+case object Owner extends RoomRole
+case object Member extends RoomRole
+
+case class RoomMember(
+                     roomId: UUID,
+                     userId: UUID,
+                     role: RoomRole,
+                     joinedAt: Instant
+                     )
+
+sealed trait Difficult
+case object Easy extends Difficult
+case object Medium extends Difficult
+case object Hard extends Difficult
+
+case class Task(
+               id: UUID,
+               title: String,
+               description: String,
+               difficulty: Difficult,
+               category: Option[String],
+               roomId: UUID,
+               createdBy: UUID,
+               createdAt: Instant,
+               isActive: Boolean
+               )
+
+sealed trait EventStatus
+case object Completed  extends EventStatus
+case object Skipped    extends EventStatus
+case object InProgress extends EventStatus
+
+case class TaskEvent(
+                      id: UUID,
+                      taskId: UUID,
+                      userId: UUID,
+                      roomId: UUID,
+                      status: EventStatus,
+                      comment: Option[String],
+                      occurredAt: Instant
+                    )
